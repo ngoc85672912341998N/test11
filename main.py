4e8d253492f90787d6972355a4cc3bd27aa65d71
ngoc856729 <122215302+ngoc856729@users.noreply.github.com>
2023-07-28 16:09:25 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+import time
+from typing import Optional
+from sqlmodel import Field, Session, SQLModel, create_engine, select
+from models import bang_nhan_vien,update_data
+from sqlmodel import Session,select
+from sql_model import engine
+from sqlmodel import Field, Session, SQLModel, create_engine, select
+import os
+from fastapi import BackgroundTasks, FastAPI
 import uvicorn
-from fastapi import FastAPI
+session=Session(bind=engine)
 
 app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+templates = Jinja2Templates(directory="templates")
 
 
 @app.get("/")
