9500a3a1f14e9771094e7e582b637c787318c3d6
ngoc856729 <122215302+ngoc856729@users.noreply.github.com>
2023-07-28 16:01:25 +0700

    Update main.py

--- main.py
+++ main.py
@@ -58,4 +58,4 @@ async def root():
     return {"message": "Hello from FastAPI!"}
 
 if __name__ == "__main__":
-    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
